97375b6b5e7da6ae42d44da0e05d9312e83e7a62
Elliot Wolfe & Danny Pham <pair+Wolfy858.dannynpham@devbootcamp.com>
2016-05-12 08:58:33 -0700

    refactor board method

--- battleship.rb
+++ battleship.rb
@@ -1,35 +1,23 @@
 require 'byebug'
 #method to populate a board w/ coordinates
 def board
-count = 0
-board = []
-
-100.times do
+  count = 0
+  count10 = 10
+  board = []
+  letter = "a"
+  100.times do
   count += 1
   case count
-  when (1..10)
-    board << "a#{count}"
-  when (11..20)
-    board << "b#{count-10}"
-  when (21..30)
-    board << "c#{count-20}"
-  when (31..40)
-    board << "d#{count-30}"
-  when (41..50)
-    board << "e#{count-40}"
-  when (51..60)
-    board << "f#{count-50}"
-  when (61..70)
-    board << "g#{count-60}"
-  when (71..80)
-    board << "h#{count-70}"
-  when (81..90)
-    board << "i#{count-80}"
-  when (91..100)
-    board << "j#{count-90}"
+  when count..count10
+    board << letter + "#{count}"
+    count10 += 10 + count
+    if count == 10
+      count = 0
+      letter = letter.next
+    end
   end
-end
-array.each_slice(10).to_a
+  end
+board.each_slice(10).to_a
 end
 
 def players_shot
